a94ed1a7deb058f93a5cad9c41ac202885a4d680
Justin Kahn <lirioteam@gmail.com>
2015-08-07 18:04:10 -0400

    remove console.logs and some more unused controllers

--- platforms/ios/www/js/controllers/collections.js
+++ platforms/ios/www/js/controllers/collections.js
@@ -11,44 +11,36 @@ angular.module('train.controllers.collections', [
   'ngSanitize'
 ])  // Playlist Controller
 .controller('PlaylistCtrl', function ($scope, $state, $stateParams, $ionicNavBarDelegate, $ionicScrollDelegate, NavBarService, CollectionsDBFactory) {
+
   var showDetails = {};
   NavBarService.setTransparency(true);
   $scope.playlist = [];
-  // array of objects each with collection and nextStep
   $scope.collections = [];
+
   $scope.updatePlaylist = function () {
-    console.log('update playlist');
     CollectionsDBFactory.allCollections().then(function (collections) {
       $scope.collections = collections;
-      console.log('allcollections');
-      console.log(collections);
+
       for (i = 0; i < $scope.collections.length; i++) {
-        console.log('for loop');
         var col = $scope.collections[i];
-        console.log('col ' + col.Title);
         $scope.setCollectionStep(col, i);
       }
     });
   };
-  $scope.setCollectionStep = function (collection, playlistIndex) {
+
+      $scope.setCollectionStep = function (collection, playlistIndex) {
     curStepIndex = collection.CurrentStepIndex;
-    console.log('curstepindex: ' + curStepIndex);
     var steps = collection.Steps.split(',');
     var currentStepURI = steps[collection.CurrentStepIndex];
-    console.log('currentStepURI: ' + currentStepURI);
     CollectionsDBFactory.getCollectionStep(currentStepURI).then(function (step) {
       var playlistItem = {
         'collection': collection,
         'nextStep': step
       };
       $scope.playlist[playlistIndex] = playlistItem;
-      console.log('Playlist item: ' + playlistItem);
-      console.log('Playlist index: ' + playlistIndex);
-      console.log('Playlist item: ' + collection.Title + ' - ' + step.Title);
     });
   };
   $scope.updatePlaylist();
-  //$scope.currentStepDetails = {};
   $scope.onStepDetailClick = function (index) {
     if (showDetails[index] === true) {
       showDetails[index] = false;
@@ -68,16 +60,13 @@ angular.module('train.controllers.collections', [
       showDetails[index] = true;
     }
     $ionicScrollDelegate.resize();
-    console.log('ionic scroll');
     $ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
   };
   $scope.showStepDetails = function (index) {
     return showDetails[index];
   };
   $scope.showCollection = function (index) {
-    console.log('collection index: ' + index);
     var collection = $scope.playlist[index].collection;
-    console.log('collectionId: ' + collection.URI);
     $state.go('app.playlist-collection', { 'collectionId': collection.URI });
   };
 })
@@ -89,8 +78,6 @@ angular.module('train.controllers.collections', [
   $scope.updateCollection = function () {
     CollectionsDBFactory.getCollection($scope.collectionURI).then(function (collection) {
       $scope.collection = collection;
-      console.log('Collection: ' + collection);
-      console.log('Collection URI: ' + collection.URI);
       CollectionsDBFactory.getStepsForCollection(collection.URI).then(function (steps) {
         $scope.steps = steps;
         $scope.stepProcessing();
@@ -100,16 +87,13 @@ angular.module('train.controllers.collections', [
   $scope.updateCollection();
   $scope.stepProcessing = function () {
     $scope.sortSteps();
-    console.log('completed ' + $scope.completedSteps);
     for (i = 0; i < $scope.completedSteps.length; i++) {
       var step = $scope.completedSteps[i];
       step.detailDisplay = false;
-      console.log('step.detailDisplay ' + step.detailDisplay);
     }
     for (i = 0; i < $scope.nextSteps.length; i++) {
       var step = $scope.completedSteps[i];
       step.detailDisplay = false;
-      console.log('step.detailDisplay ' + detailDisplay);
     }
   };
   $scope.sortSteps = function () {
@@ -117,7 +101,6 @@ angular.module('train.controllers.collections', [
     $scope.completedSteps = $scope.steps.slice(0, currentStep);
     $scope.nextSteps = $scope.steps.slice(currentStep, $scope.steps.length);
     $scope.completedStepCount = currentStep;
-    console.log('sort steps: ' + currentStep);
   };
   $scope.iconsDict = {
     Recording: '/img/CollectionIcons/recording.png',
@@ -125,27 +108,22 @@ angular.module('train.controllers.collections', [
   };
   function icon(type) {
     if (type == Recording) {
-      console.log('recording');
       return '/img/CollectionIcons/recording.png';
     }
     if (type == Article) {
-      console.log('article');
       return '/img/CollectionIcons/article.png';
     }
-    console.log('icon');
   }
   var videos = [];
   $scope.videos = [];
   $scope.clip = '';
   $scope.captureVideo = function () {
-    console.log('capture video');
     $cordovaCapture.captureVideo().then(function (videoData) {
       VideoService.saveVideo(videoData).success(function (data) {
         $scope.clip = data;
         $scope.afterCapture();
         $scope.$apply();
       }).error(function (data) {
-        console.log('ERROR: ' + data);
       });
     });
   };
@@ -167,39 +145,29 @@ angular.module('train.controllers.collections', [
   };
   $scope.loadVideos();
   $scope.captureVideo = function () {
-    console.log('capture video');
     $cordovaCapture.captureVideo().then(function (videoData) {
       VideoService.saveVideo(videoData).success(function (data) {
         $scope.clip = data;
-        console.log('video data: ' + data);
         $scope.afterCapture(data);
         $scope.$apply();
       }).error(function (data) {
-        console.log('ERROR: ' + data);
       });
     });
   };
   $scope.afterCapture = function (data) {
     //$scope.step.Items = data;
     CollectionsDBFactory.setCollectionStepItems($scope.step, data).then(function () {
-      //console.log("after Catpture: " + data);
       $scope.step.Items = data;
       $scope.videoURL = $scope.video.LocalMediaURL;
     });
-    console.log('scope.step: ' + $scope.step);
   };
   $scope.display = false;
   $scope.detailDisplay = function () {
     $scope.display = !$scope.display;
     $ionicScrollDelegate.resize();
-    console.log('ionic scroll');
     $ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom(true);
   };
   $scope.showVideo = function () {
-    console.log('show video');
-    console.log($scope.step.Items);
-    console.log($scope.collectionId);
-    console.log($state.current.name);
     var vid = $scope.step.Items;
     var collection = $scope.collectionId;
 
@@ -212,7 +180,6 @@ angular.module('train.controllers.collections', [
     return 1 + parseInt(curIndex) + parseInt($scope.stepNumber) + ' ';
   };
 }).controller('VideogularPlayerCtrl', function ($scope, $stateParams, $sce, MediaDBFactory) {
-  console.log('VideogularPlayerCtrl');
   $scope.filename = $stateParams.videoId;
   var filename = $scope.filename;
   $scope.config = {
@@ -239,14 +206,9 @@ angular.module('train.controllers.collections', [
   MediaDBFactory.getMedia(filename).then(function (data) {
     $scope.video = data;
     $scope.videoURL = $scope.video.LocalMediaURL;
-    console.log('filename: ' + filename);
-    console.log('videoURL: ' + $scope.videoURL);
     var resourceURL = $sce.trustAsResourceUrl($scope.videoURL);
-    console.log('resourceURL: ' + resourceURL);
     $scope.config.videos[0].src = $sce.trustAsResourceUrl($scope.videoURL);
     $scope.config.videos[0].type = 'video/mp4';
-    console.log('filename: ' + filename);
-    console.log('videoURL: ' + $scope.videoURL);
   });
 }).controller('RecordingPlayerCtrl', function ($scope, $timeout, $ionicScrollDelegate, $ionicHistory, $stateParams, MediaDBFactory) {
   $scope.comments = [];
@@ -258,7 +220,6 @@ angular.module('train.controllers.collections', [
     $scope.poster = $scope.video.LocalThumbnailURL;
     $scope.commentArray = $scope.video.Comments.split(',');
     $scope.comments = parseComments($scope.commentArray);
-    console.log($scope.videoURL);
   });
   function parseComments(commentArray) {
     var newCommArray = [];
@@ -274,7 +235,6 @@ angular.module('train.controllers.collections', [
   $scope.hideTime = true;
   var alternate, isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
   $scope.sendMessage = function () {
-    console.log('send message');
     alternate = !alternate;
     var d = new Date();
     d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
@@ -282,10 +242,8 @@ angular.module('train.controllers.collections', [
       commenter: alternate ? 'Justin' : 'Sarah',
       comment: $scope.data.message
     };
-    console.log('new comm: ' + newComm.comment);
     $scope.comments.push(newComm);
     $scope.$apply();
-    console.log('Comments: ' + $scope.comments.length);
     delete $scope.data.message;
     $ionicScrollDelegate.scrollBottom(true);
   };
@@ -318,9 +276,6 @@ angular.module('train.controllers.collections', [
     $scope.display = !$scope.display;
   };
   $scope.showVideo = function () {
-    console.log($scope.step.Items);
-    console.log($scope.collectionId);
-    console.log($state.current.name);
     var vid = $scope.step.Items;
     var collection = $scope.collectionId;
     $state.go('app.playlist-collection-video', {
@@ -349,7 +304,6 @@ angular.module('train.controllers.collections', [
   $scope.hideTime = true;
   var alternate, isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
   $scope.sendMessage = function () {
-    console.log('send message');
     alternate = !alternate;
     var d = new Date();
     d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
@@ -357,10 +311,8 @@ angular.module('train.controllers.collections', [
       commenter: alternate ? 'Justin' : 'Sarah',
       comment: $scope.data.message
     };
-    console.log('new comm: ' + newComm.comment);
     $scope.comments.push(newComm);
     $scope.$apply();
-    console.log('Comments: ' + $scope.comments.length);
     delete $scope.data.message;
     $ionicScrollDelegate.scrollBottom(true);
   };
@@ -394,10 +346,6 @@ angular.module('train.controllers.collections', [
     $scope.display = !$scope.display;
   };
   $scope.readArticle = function () {
-    console.log('read article');
-    console.log($scope.step.Items);
-    console.log($scope.collectionId);
-    console.log($state.current.name);
     var article = $scope.step.Items;
     var collection = $scope.collectionId;
     $state.go('app.playlist-collection-article', {
@@ -409,26 +357,18 @@ angular.module('train.controllers.collections', [
     return 1 + parseInt(curIndex) + parseInt($scope.stepNumber) + ' ';
   };
   $scope.isLink = function (str) {
-    console.log('is link: ' + str);
-    console.log('pattern recognized:');
     if (!/^(f|ht)tps?:\/\//i.test(str)) {
-      console.log('is link false');
       return false;
     } else {
-      console.log('is link true');
       return true;
     }
   };
 }).controller('ArticleCtrl', function ($scope, $stateParams) {
   $scope.article = $stateParams.article;
   $scope.isLink = function (str) {
-    console.log('is link: ' + str);
-    console.log('pattern recognized:');
     if (!/^(f|ht)tps?:\/\//i.test(str)) {
-      console.log('is link false');
       return false;
     } else {
-      console.log('is link true');
       return true;
     }
   }  /*
@@ -455,10 +395,6 @@ angular.module('train.controllers.collections', [
     },
     controller: 'RecordingStepCtrl',
     link: function (scope, element, attrs) {
-      console.log('recordingStep');
-      console.log('recordingStep: ' + scope.step);
-      console.log('recordingStep-Items: ' + scope.step.Items);
-      console.log('parentIndex: ' + scope.parentIndex);
     },
     templateUrl: 'templates/directives/step-recording.html'
   };
@@ -473,10 +409,6 @@ angular.module('train.controllers.collections', [
     },
     controller: 'ArticleStepCtrl',
     link: function (scope, element, attrs) {
-      console.log('recordingStep');
-      console.log('recordingStep: ' + scope.step);
-      console.log('recordingStep-Items: ' + scope.step.Items);
-      console.log('parentIndex: ' + scope.parentIndex);
     },
     templateUrl: 'templates/directives/step-Article.html'
   };
@@ -491,7 +423,6 @@ angular.module('train.controllers.collections', [
     },
     controller: 'VideoStreamingStepCtrl',
     link: function (scope, element, attrs) {
-      console.log('videoStep');
     },
     templateUrl: 'templates/directives/step-video.html'
   };
